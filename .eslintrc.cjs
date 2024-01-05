a56ffd9293afe8bc06fbe3a42aea56c1b35c0f03
DoubleUTH <154479212+doubleuth@users.noreply.github.com>
2024-01-05 20:57:26 +0800

    Minor fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -39,7 +39,7 @@ module.exports = {
             },
         },
         {
-            files: ["app/**"],
+            files: ["web/**"],
             env: {
                 browser: true,
             },

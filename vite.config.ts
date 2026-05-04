12292e80f50d0d48cbfd7d385bf86b9c1ba96d3e
DoubleUTH <154479212+DoubleUTH@users.noreply.github.com>
2026-05-04 23:31:29 +0800

    Update node packages minor versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite"
 import solid from "vite-plugin-solid"
-import path from "path"
+import path from "node:path"
 
 export default defineConfig({
     root: path.resolve(process.cwd(), "web"),

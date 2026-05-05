733f9b5068cc9ab80601fabc69c499efb088caa2
DoubleUTH <154479212+DoubleUTH@users.noreply.github.com>
2026-05-05 15:48:11 +0800

    Add native mode override

--- web/src/worldgen.ts
+++ web/src/worldgen.ts
@@ -4,6 +4,8 @@ import { WorldGenNative } from "./worldgen/native"
 const browser: WorldGen = new WorldGenBrowser()
 const native: WorldGen = new WorldGenNative()
 
+const ALWAYS_NATIVE = false
+
 export function getWorldGen(nativeMode: boolean): WorldGen {
-    return nativeMode ? native : browser
+    return ALWAYS_NATIVE ? native : nativeMode ? native : browser
 }
